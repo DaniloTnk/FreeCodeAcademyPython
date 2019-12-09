60192f2b3915c6510bf4ed1ac22fa300d97a97e2
Tanaka <danilocaceres@gmail.com>
2019-12-08 21:24:50 -0300

    George's Tale

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-print("/___|")
-print("   /|")
-print("  / |")
-print(" /  |")
+print("There once was a man named George,")
+print("he was 70 years old.")
+print("He really liked the name George.")
+print("but didn't like being 70.")
