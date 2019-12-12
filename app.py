7f7f4f004bc8c2372fa299f6a19c17dae75519e0
Tanaka <danilocaceres@gmail.com>
2019-12-12 18:38:26 -0300

    Import math module and using floor and ceil functions

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-my_number = -5
-print(abs(my_number)) # Return 5
-print(pow(3, 2))  # Return 9
-print(pow(6, 4))  # Return 1296, 6*6*6*6
-print(max(6, 4))  # Return 6
-print(min(6, 4))  # Return 4
-print(round(3.3)) # Return 3
-print(round(3.7)) # Return 4
+from math import * # This line allow us to use functions of library math
+print(floor(3.7))  # Return 3
+print(floor(2.9))  # Retutn 2
+print(floor(-5.5)) # Return -6 the next lowest integer value
+print(ceil(3.7))   # Return 4
+print(ceil(2.9))   # Return 3
+print(ceil(-5.5))  # Return -5 the next largest integer value
