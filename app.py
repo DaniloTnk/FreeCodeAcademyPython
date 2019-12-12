fa77f965ca1a55ce9d01bc137b6e19a8f249b5f1
Tanaka <danilocaceres@gmail.com>
2019-12-12 15:34:39 -0300

    Testingr replace function.

--- app.py
+++ app.py
@@ -1,9 +1,2 @@
 phrase = "Giraffe Academy"
-print(phrase[0]) # To get the first character of phrase the captal G.
-print(phrase[3]) # Get the fourth character of phrase the first a.
-print(phrase.index("G")) # Show the index of the captal G.
-print(phrase.index("a")) # Show the index of the first a.
-print(phrase.index("Acad")) # Show the index where our string start.
-print(phrase.index("z")) # Bring an error because phrase don't have any z.
-
-
+print(phrase.replace("Giraffe","Elephant"))
