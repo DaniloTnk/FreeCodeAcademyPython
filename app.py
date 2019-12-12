9c58eb4e699e03a5a8722f48152b4fc67d6048fc
Tanaka <danilocaceres@gmail.com>
2019-12-12 17:26:41 -0300

    Basic numbers functions.

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-my_number = 5
-print(my_number)
-print(str(my_number) + " my favorite number.")
-print(my_number + " my favorite number.") # Return error because we don't convert our number into string.
+my_number = -5
+print(abs(my_number)) # Return 5
+print(pow(3, 2))  # Return 9
+print(pow(6, 4))  # Return 1296, 6*6*6*6
+print(max(6, 4))  # Return 6
+print(min(6, 4))  # Return 4
+print(round(3.3)) # Return 3
+print(round(3.7)) # Return 4
