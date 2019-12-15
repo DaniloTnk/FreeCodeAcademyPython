664a4262764ecf1edf5ad56fddc3886e368e59bf
Tanaka <danilocaceres@gmail.com>
2019-12-15 16:47:59 -0300

    Crate functions and change the return

--- calculator.py
+++ calculator.py
@@ -1,15 +1,27 @@
 expression = input("Type your math expression: ")
 
-first_number = expression[0]
+def add(first, second):
+    return first + second
+
+def subtract(first, second):
+    return first - second
+
+def multiply(first, second):
+    return first * second
+
+def divide(first, second):
+    return first / second
+
+first_number = int(expression[0])
 operator = expression[1]
-second_number = expression[2]
+second_number = int(expression[2])
 expression += "="
 
 if operator =="+":
-     print( expression + str(int(first_number) + int(second_number)))
+     print( expression + str(sum(first_number, second_number)))
 elif operator == "-":
-    print(expression + str(int(first_number) - int(second_number)))
+    print(expression + str(subtract(first_number, second_number)))
 elif operator == "*":
-    print(expression + str(int(first_number) * int(second_number)))
+    print(expression + str(multiply(first_number, second_number)))
 elif operator == "/":
-    print(expression + str(int(first_number) / int(second_number)))
+    print(expression + str(divide(first_number, second_number)))
