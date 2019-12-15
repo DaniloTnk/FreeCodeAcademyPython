b6a6aa22335f94c6c6b37b9a9d6f744febf183d7
Tanaka <danilocaceres@gmail.com>
2019-12-15 00:20:45 -0300

    Square root function.

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from math import * # This line allow us to use functions of library math
-print(floor(3.7))  # Return 3
-print(floor(2.9))  # Retutn 2
-print(floor(-5.5)) # Return -6 the next lowest integer value
-print(ceil(3.7))   # Return 4
-print(ceil(2.9))   # Return 3
-print(ceil(-5.5))  # Return -5 the next largest integer value
+print(sqrt(16)) # Return 4.
+print(sqrt(25)) # Return 5.
+print(sqrt(144)) # Return 12.
