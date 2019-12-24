5b45064947784f2480c998ec4690cbc1e2c90a42
Tanaka <danilocaceres@gmail.com>
2019-12-24 15:26:35 -0300

    Change the game to recieve 3 diffets input and printed 3 times the storie with random element of a list.

--- madlibsgame.py
+++ madlibsgame.py
@@ -1,7 +1,21 @@
-color = input("Enter a color: ")
-plural_noun = input("Enter a Plural Noun: ")
-celebrity = input("Enter a celebrity: ")
+import random
 
-print("Roses are " + color)
-print(plural_noun + " are blue")
-print("I love " + celebrity)
+colors = input("Enter 3 colors separated by space: ")
+plural_nouns = input("Enter 3 Plural Nouns separated by space: ")
+celebrities = input("Enter 3 celebrities separated by space: ")
+
+colors_list = colors.split()
+plural_nouns_list = plural_nouns.split()
+celebrities_list = celebrities.split()
+
+print("Roses are " + random.choice(colors_list))
+print(random.choice(plural_nouns_list) + " are blue")
+print("I love " + random.choice(celebrities_list))
+print("")
+print("Roses are " + random.choice(colors_list))
+print(random.choice(plural_nouns_list) + " are blue")
+print("I love " + random.choice(celebrities_list))
+print("")
+print("Roses are " + random.choice(colors_list))
+print(random.choice(plural_nouns_list) + " are blue")
+print("I love " + random.choice(celebrities_list))
